68d19134fdd5f4a6db2d7aaadba778a7b75847f3
elbasurit4 <67765150+elbasurit4@users.noreply.github.com>
2022-05-08 23:58:22 -0400

    Add files via upload

--- ts/main.ts
+++ ts/main.ts
@@ -28,7 +28,7 @@ function construirNav() {
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#"><span class="bi-house-door"></span> INICIO</a>
+                <a class="nav-link" aria-current="page" href="inicio.html"><span class="bi-house-door"></span> INICIO</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="foro.html"><span class="bi-file-earmark-person"></span> FORO</a>
